d009d544686169406dadf973a2446b2d8773e84d
Lucy Farnik <lucyfarnik@gmail.com>
2023-03-30 20:58:23 +0100

    Added episodic environments

--- env.py
+++ env.py
@@ -19,7 +19,16 @@ class Env():
     self.transition_dist = transition_dist
 
 class RandomEnv(Env):
-  def __init__(self, n_s: int = 128, n_a: int = 16, discount: int = 0.9):
+  def __init__(self, n_s: int = 128, n_a: int = 16, discount: int = 0.9,
+               episodic: bool = False):
+    """
+      Create a random environment.
+      n_s: number of states
+      n_a: number of actions
+      discount: discount factor
+      episodic: whether the environment is episodic - in this case episodic
+        means that there is a terminal (self-looping) state
+    """
     # uniform init dist
     init_dist = np.ones(n_s) / n_s
 
@@ -32,5 +41,13 @@ class RandomEnv(Env):
     transition_dist = np.where(transition_dist > thresh,
                          transition_dist, np.zeros_like(transition_dist)-20)
     transition_dist = softmax(transition_dist)
+
+    self.episodic = episodic
+    if episodic:
+      self.terminal_state = np.random.randint(0, n_s)
+      for a in range(n_a):
+        for s_prime in range(n_s):
+          prob = 1.0 if s_prime == self.terminal_state else 0.0
+          transition_dist[self.terminal_state, a, s_prime] = prob
 
     super().__init__(n_s, n_a, discount, init_dist, transition_dist)
